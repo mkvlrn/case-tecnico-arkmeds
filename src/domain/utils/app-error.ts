9289602da07d1d0e56178ec2f4f74c627a0cb62d
Mike Valeriano <mkvlrn@gmail.com>
2025-10-15 14:17:26 -0300

    feat: add main domain/app error class

---
+++ src/domain/utils/app-error.ts
@@ -0,0 +1,32 @@
+import { getReasonPhrase, StatusCodes } from "http-status-codes";
+
+export type ErrorCode = "invalidInput";
+
+export class AppError extends Error {
+  static readonly errorToStatus: Record<ErrorCode, StatusCodes> = {
+    invalidInput: StatusCodes.UNPROCESSABLE_ENTITY,
+  };
+
+  readonly code: ErrorCode;
+  readonly status: string;
+  readonly statusCode: StatusCodes;
+
+  constructor(code: ErrorCode, message: string, cause?: unknown) {
+    super(message);
+    this.name = "AppError";
+    this.cause = cause;
+    this.code = code;
+
+    const statusCode = AppError.errorToStatus[code];
+    this.status = getReasonPhrase(statusCode);
+    this.statusCode = statusCode;
+  }
+
+  serialize() {
+    return {
+      code: this.code,
+      message: this.message,
+      details: this.cause,
+    };
+  }
+}
